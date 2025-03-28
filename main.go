33b8af6d3c84665901adfb37d66a120d2f450422
Matthias Baumgartner <matthias.baumgartner@sesam-ag.ch>
2025-03-28 23:01:47 +0100

    Add cors

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"beaver/inventory/config"
 	"beaver/inventory/core/domain"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -43,6 +45,14 @@ func main() {
 	}
 
 	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
+	r.Use(cors.New(config))
 	r.Use(auth.TokenCheck(IdpUrl))
 	v1 := r.Group("/v1")
 
